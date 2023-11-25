6029fc421fcf66547559d6d5d1f8b2469c9a4300
deotime <89555032+DeoTimeTheGithubUser@users.noreply.github.com>
2023-11-25 11:01:05 -0600

    what?

--- scraper/src/index.ts
+++ scraper/src/index.ts
@@ -9,6 +9,7 @@ import {Entry} from "./model";
 import * as progress from "cli-progress"
 // @ts-ignore
 import chalk from "chalk"
+import { select } from "@inquirer/prompts"
 import TagElement = cheerio.TagElement;
 
 async function checkExists(path: PathLike): Promise<boolean> {
@@ -54,19 +55,24 @@ async function indexSeason(season: number, data: cheerio.TagElement) {
 
 function createBar(
     name: string,
-    total: number
+    total: number,
+    fps = 10,
+    etaBuffer = 10
 ): progress.Bar {
     const bar = new progress.Bar({
-        format: `${chalk.green.bold(name)} | ${chalk.blue("{bar}")} | {percentage}% || {value}/{total} completed | ETA: {eta}s`,
+        format: `${chalk.yellow.bold(name)} | ${chalk.blue("{bar}")} | {percentage}% || {value}/{total} completed | Time remaining: ${chalk.cyan("{eta_formatted}")}`,
         barCompleteChar: '\u2588',
         barIncompleteChar: '\u2591',
-        hideCursor: true
+        hideCursor: true,
+        etaAsynchronousUpdate: true,
+        fps,
+        etaBuffer
     })
     bar.start(total, 0)
     return bar
 }
 
-async function exportIndexesToVectors(amount: number) {
+async function exportIndexesToVectors() {
     const indexes = path.join(__dirname, constants.index)
     const vectors = path.join(__dirname, constants.vectors)
     const numerical = (a: string, b: string) => Number(a) - Number(b)
@@ -87,6 +93,10 @@ async function exportIndexesToVectors(amount: number) {
                 const character = line.split(":")[0] ?? "Unknown"
                 const entry: Entry = {
                     text: line,
+                    ep: {
+                        season: Number(season),
+                        number: Number(episode)
+                    },
                     character,
                     responseTo: i == 0 ? undefined : entries.length - 1,
                     context
@@ -98,32 +108,35 @@ async function exportIndexesToVectors(amount: number) {
         }
     }
     parseBar.stop()
-    const embedBar = createBar("Creating Vectors", amount)
-    const data: string[] = []
+    const embedBar = createBar("Creating Vectors", entries.length, 0.1, 100)
+    await fs.writeFile(vectors, "")
     let id = 0
     for (const chunk of utils.chunked(entries, constants.embeddingBatchSize)) {
         const embeddings = await utils.ai.createEmbedding(chunk.map(e => e.text), {
             cloudflareAccountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
             cloudflareApiKey: process.env.CLOUDFLARE_API_KEY!
         })
-        data.push(...utils.zip(chunk, embeddings)
+        const data = utils.zip(chunk, embeddings)
             .map(([entry, values], i) => JSON.stringify({
                 id: `${id + i}`,
                 values,
-                metadata: entry
-            })))
+                metadata: entry,
+                namespace: `${entry.ep.season}:${entry.ep.number}`
+            }))
+        const joined = data.join("\n")
+        await fs.appendFile(vectors, id === 0 ? joined : `\n${joined}`)
         id += chunk.length
         embedBar.update(id)
-        if (id >= amount) break
     }
     embedBar.stop()
-    await fs.writeFile(vectors, data.join("\n"))
+    console.log(chalk.green.bold("Successfully exported vectors."))
 }
 
 async function main() {
 
+    return
     if (!await checkExists(path.join(__dirname, constants.index))) {
-        console.log("Indexes not already made, making them now...")
+        console.log("Indexes not already made, creating them now...")
         const page = await fetch(constants.webIndex)
         const $ = cheerio.load(await page.text())
         $(constants.selectors.seasons)
@@ -131,9 +144,14 @@ async function main() {
             .slice(0, -1)
             .map((e, i) => indexSeason(i + 1, e as TagElement))
     }
-    await exportIndexesToVectors(1000)
+    await exportIndexesToVectors()
 
 }
 
 require("dotenv").config()
+let config
+(async () => {
+    config = {
+    }
+})()
 void main()
--- scraper/src/model.ts
+++ scraper/src/model.ts
@@ -1,6 +1,12 @@
 export type Entry = {
     text: string, // what was said
+    ep: Episode,
     character: string, // character who speaks
     responseTo?: number, // entry that this was in response to
     context?: string // context of this entry
+}
+
+export type Episode = {
+    season: number,
+    number: number
 }
