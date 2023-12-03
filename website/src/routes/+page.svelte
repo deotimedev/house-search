4a4e46d0df1df64123935b68fe27cdfb5022aba1
deotime <89555032+DeoTimeTheGithubUser@users.noreply.github.com>
2023-12-03 00:40:48 -0600

    update

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,55 +1,66 @@
 <script lang="ts">
     import Title from "$lib/components/Title.svelte";
-    import {fly} from "svelte/transition"
-    import {onMount} from "svelte";
+    import { fly } from "svelte/transition";
+    import { onMount } from "svelte";
     import Search from "$lib/components/Search.svelte";
     import Entry from "$lib/components/Entry.svelte";
-    import type {ScoredEntry} from "./search/SearchResults";
+    import type { ScoredEntry } from "./search/SearchResults";
     import _ from "lodash";
+    import { SyncLoader } from "svelte-loading-spinners";
     import SocialIcon from "$lib/components/SocialIcon.svelte";
 
-    let ready = false
-    onMount(() => ready = true)
+    let ready = false;
+    onMount(() => (ready = true));
 
-    let results: Promise<ScoredEntry[]> | undefined
+    let results: Promise<ScoredEntry[]> | undefined;
 </script>
-
 
 {#if ready}
     <div class="flex justify-end pr-[2vw] pt-[2vw]">
-        <SocialIcon 
+        <SocialIcon
             type="github"
             username="@deotimedev"
             link="https://github.com/deotimedev/house-search"
         />
     </div>
-    <div class="flex flex-col items-center space-y-5 pt-[15.6vh]" style="height: 100%">
-        
-        <div transition:fly={{ y: 100, duration: 1000 }}><Title/></div>
+    <div
+        class="flex flex-col items-center space-y-5 pt-[15.6vh]"
+        style="height: 100%"
+    >
+        <div transition:fly={{ y: 100, duration: 1000 }}><Title /></div>
         <div transition:fly={{ y: 200, duration: 1000 }}>
-            <Search on:search={(e) => {
-                const query = e.detail.query
-                console.log(`The query in question: ${query}`)
-                results = (async () => {
-                    const res = await fetch(`/search?q=${query}`)
-                    return await res.json()
-                })()
-            }}/>
+            <Search
+                on:search={(e) => {
+                    const query = e.detail.query;
+                    console.log(`The query in question: ${query}`);
+                    results = (async () => {
+                        const res = await fetch(`/search?q=${query}`);
+                        return await res.json();
+                    })();
+                }}
+            />
         </div>
 
-        <!-- {#if results}
+        {#if results}
             {#await results}
-                <SyncLoader size="60" color="#FFFFFF" unit="px" duration="0.5s"/>
+                <SyncLoader size="60" color="#FFFFFF" unit="px" duration="0.5s" />
             {:then results}
-                {#each (_.orderBy(results, "score", "desc")) as result, i}
-                    <div in:fly|global={{ y: 150, duration: 1000, delay: (i * 100) }} class="min-w-[100%] flex items-center flex-col">
+                {#each _.orderBy(results, "score", "desc") as result, i}
+                    <div
+                        in:fly|global={{
+                            y: 150,
+                            duration: 1000,
+                            delay: i * 100,
+                        }}
+                        class="min-w-[100%] flex items-center flex-col"
+                    >
                         <Entry entry={result} />
                     </div>
                 {/each}
             {/await}
-        {/if} -->
+        {/if}
 
-         {#each [1, 2, 3, 4] as n}
+        <!-- {#each [1, 2, 3, 4] as n}
             <div class="flex items-center flex-col">
                 <Entry entry={{
                             text: "(shouting) Life is pain! I wake up every morning, I'm in pain. I go to work in pain. You know how many times I wanted to just give up? How many times I thought about ending it?",
@@ -70,7 +81,6 @@
                             }
                         }} />
             </div>
-        {/each} 
+        {/each}  -->
     </div>
 {/if}
-
